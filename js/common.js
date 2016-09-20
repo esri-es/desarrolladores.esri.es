34e791d8208d03fe38691b3fc7a840af6087d16a
RaulJimenezOrtega <hhkaos@gmail.com>
2016-09-20 21:21:09 +0200

    Adding mooc lesson video player

--- js/common.js
+++ js/common.js
@@ -21,3 +21,53 @@ $(function() {
     localStorage.acceptedTos = true;
   });
 });
+
+$(".enroll-btn").click(function(e){
+  // Open modal
+  document.getElementById('enrollCourse').click();
+});
+
+$(".js-modal .js-modal-toggle").click(function(e){
+  var modal = $(this).data("click");
+  if(modal){
+    document.getElementById(modal).click();
+  }
+});
+
+$(document).ready(function(){
+  $('.video-index tbody tr').each(function(i, elem){
+    //debugger;
+    var el = $(elem).children()[0];
+
+    $(el).parent().attr("data-seconds", parseToSeconds($(el).text()));
+  });
+
+  $('.video-index tbody tr').click(function(){
+    player.seekTo($(this).data("seconds"));
+  });
+
+  $("#prev").click(function(){
+    var el = $(".side-nav .is-active")[0];
+    var prev = $(el).prev();
+    if(!prev.attr("href")){
+      prev = $(prev).prev();
+    }
+    redirect(prev.attr("href"));
+  });
+  $("#next").click(function(){
+    var el = $(".side-nav .is-active")[0];
+    var next = $(el).next();
+    if(!next.attr("href")){
+      next = $(next).next();
+    }
+    redirect(next.attr("href"));
+  });
+
+  function redirect(slug){
+    var a=window.location.href.split("/")
+    a.pop()
+    a.push(slug)
+    window.location= a.join("/");
+  }
+
+});
